f9b4e6f610c912b5e0dfb50697ac0dcf2e8e3fca
Margarita Kuznetsova <margarita.kuznetsova@inf.ethz.ch>
2022-01-21 10:47:16 +0100

    tests for delete_short_vent_events

--- tests/endpoints/test_endpoints.py
+++ tests/endpoints/test_endpoints.py
@@ -28,7 +28,7 @@ def test_kernel_smooth_arr():
 
     input_arr_2 = np.array([1.3, 2.5, 3.6])
     bandwidth_2 = 2.5
-    correct_formula = np.array([1.44368064, 2.4893493 , 3.46823613])
+    correct_formula = np.array([1.4436806438153837, 2.48934930210808 , 3.468236127284816])
     
     res_2 = endpoint_benchmark.kernel_smooth_arr(input_arr_2, bandwidth_2)
     assert np.all(res_2 == correct_formula)
@@ -36,7 +36,7 @@ def test_kernel_smooth_arr():
 
 def test_merge_short_vent_gaps():
     #TO DO: Positions which are not gaps are never modified - smth wrong with this
-
+    #verify 1 or 12?
     #gaps of larger length than short_gap_hours are not removed
 
     vent_status_arr_1 = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0])
@@ -60,7 +60,32 @@ def test_merge_short_vent_gaps():
     assert np.all(status_3 == vent_status_arr_3)
 
 
+def test_delete_short_vent_events():
+    #TO DO: verify about equal (!)
+
+    #Short events shorted than the threshold are indeed deleted
+    vent_status_arr_1 = np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0])
+    short_event_hours_ = 1
+    corrected_events = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    res_1 = endpoint_benchmark.delete_short_vent_events(vent_status_arr_1, short_event_hours_)
+    assert np.all(res_1 == corrected_events)
+
+    #Events longer or equal than the threshold are not deleted
+    vent_status_arr_2 = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0])
+    res_2 = endpoint_benchmark.delete_short_vent_events(vent_status_arr_2, short_event_hours_)
+
+    assert np.all(res_2 == vent_status_arr_2)
+
+    #Non-events are never modified
+    vent_status_arr_3 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    res_3 = endpoint_benchmark.delete_short_vent_events(vent_status_arr_3, short_event_hours_)
+    assert np.all(res_3 == vent_status_arr_3)
+
 def test_mix_real_est_pao2():
+
+    #TO DO: The correct closest measurement is found in the first inner loop
+
     pao2_col_1 = np.array([200, 150, 170.1, 200.3])
     pao2_meas_cnt_1 = np.array([1, 4, 5, 6])
     pao2_est_arr_1 = np.array([198.3, 160.2, 175.3, 199.5])
